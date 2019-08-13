2f69ba7984899b1d68a4e9deb3c50d8d8cdc38ca
YeJ XU <nhxyj111@gmail.com>
2019-08-13 20:00:28 +0800

    8

---
+++ schema/resolvers.ts
@@ -0,0 +1,54 @@
+import { GraphQLDateTime } from 'graphql-iso-date'
+import { chats, messages } from '../db'
+
+const resolvers = {
+  Date: GraphQLDateTime,
+
+  Chat: {
+    messages(chat: any) {
+      return messages.filter(m => chat.messages.includes(m.id))
+    },
+    lastMessage(chat: any) {
+      const lastMessage = chat.messages[chat.messages.length - 1]
+      return messages.find(m => m.id === lastMessage)
+    },
+  },
+
+  Query: {
+    chats() {
+      return chats
+    },
+    chat(root: any, { chatId }: any) {
+      return chats.find(c => c.id === chatId)
+    },
+  },
+
+  Mutation: {
+    addMessage(root: any, { chatId, content }: any) {
+      const chatIndex = chats.findIndex(c => c.id === chatId)
+
+      if (chatIndex === -1) return null
+
+      const chat = chats[chatIndex]
+
+      const messagesIds = messages.map(currentMessage =>
+        Number(currentMessage.id)
+      )
+      const messageId = String(Math.max(...messagesIds) + 1)
+      const message = {
+        id: messageId,
+        createdAt: new Date(),
+        content,
+      }
+
+      messages.push(message)
+      chat.messages.push(messageId)
+      chats.splice(chatIndex, 1)
+      chats.unshift(chat)
+
+      return message
+    },
+  },
+}
+
+export default resolvers
