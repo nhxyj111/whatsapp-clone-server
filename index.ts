7bf029f225740617a1cde69db151c848675bdfd7
YeJ XU <nhxyj111@gmail.com>
2019-08-14 08:11:35 +0800

    10

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
-import { ApolloServer, gql } from 'apollo-server-express'
+import { ApolloServer, gql, PubSub } from 'apollo-server-express'
 import express from 'express'
 import cors from 'cors'
+import http from 'http'
 import { chats } from './db'
 import schema from './schema'
 const app = express()
@@ -16,15 +17,22 @@ app.get('/chats', (req, res) => {
   res.json(chats)
 })
 
-const server = new ApolloServer({ schema })
+const pubsub = new PubSub()
+const server = new ApolloServer({
+  schema,
+  context: () => ({ pubsub }),
+})
 
 server.applyMiddleware({
   app,
   path: '/graphql',
 })
 
+const httpServer = http.createServer(app)
+server.installSubscriptionHandlers(httpServer)
+
 const port = process.env.PORT || 4000
 
-app.listen(port, () => {
+httpServer.listen(port, () => {
   console.log(`Server is listening on port ${port}`)
 })
